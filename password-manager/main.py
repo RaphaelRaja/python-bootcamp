138fc032a891a7666fbdfb2075fb289e1909be70
Raphael Raja <jrrphonix@gmail.com>
2022-04-15 23:24:15 +0530

    Password Manager - Password generator functionality is completed

--- password-manager/main.py
+++ password-manager/main.py
@@ -1,8 +1,38 @@
 from tkinter import *
 from tkinter import messagebox
+import random
 
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+def generate_password():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+               'v',
+               'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q',
+               'R',
+               'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    nr_letters = random.randint(8, 10)
+    nr_symbols = random.randint(2, 4)
+    nr_numbers = random.randint(2, 4)
+
+    password_list = []
+
+    password_list += [random.choice(letters) for char in range(nr_letters)]
+
+    password_list += [random.choice(symbols) for char in range(nr_symbols)]
+
+    password_list += [random.choice(numbers) for char in range(nr_numbers)]
+
+    random.shuffle(password_list)
+
+    password = "".join(password_list)
+
+    print(f"Your password is: {password}")
+
+    password_input.insert(0, str(password))
+
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save():
@@ -10,17 +40,22 @@ def save():
     email_username = email_username_input.get()
     password = password_input.get()
 
-    is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered: \nEmail: {email_username}"
-                                                          f" \nPassword: {password} \n Is it ok to save?")
+    if len(website) == 0 or len(password) == 0:
+        messagebox.showinfo(title="Oops!", message="Please don't leave any fields empty!")
+    else:
 
-    data = website + " | " + email_username + " | " + password + "\n"
+        is_ok = messagebox.askokcancel(title=website,
+                                       message=f"These are the details entered: \nEmail: {email_username}"
+                                               f" \nPassword: {password} \n Is it ok to save?")
 
-    if is_ok:
-        with open("data.txt", "a") as f:
-            f.write(data)
-            website_input.delete(0, 'end')
-            password_input.delete(0, 'end')
-            website_input.focus()
+        data = website + " | " + email_username + " | " + password + "\n"
+
+        if is_ok:
+            with open("data.txt", "a") as f:
+                f.write(data)
+                website_input.delete(0, 'end')
+                password_input.delete(0, 'end')
+                website_input.focus()
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -53,7 +88,7 @@ password_label.grid(row=3, column=0)
 password_input = Entry()
 password_input.grid(row=3, column=1, sticky="EW")
 
-password_button = Button(text="Generate Password")
+password_button = Button(text="Generate Password", command=generate_password)
 password_button.grid(row=3, column=2, sticky="EW")
 
 add_button = Button(text="Add", width=36, command=save)
