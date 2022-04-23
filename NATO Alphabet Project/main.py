6dd977d334c3e2a254a9b6bca65dd15a68fa7f57
Raphael Raja <jrrphonix@gmail.com>
2022-04-23 17:46:57 +0530

    NATO Alphabet Project - Exception handling added

--- NATO Alphabet Project/main.py
+++ NATO Alphabet Project/main.py
@@ -6,7 +6,16 @@ print(data)
 nato_dict = {row.letter: row.code for (index, row) in data.iterrows()}
 print(nato_dict)
 
-user_word = input("Enter a word:").upper()
 
-phonetic_list = [nato_dict[word] for word in user_word]
-print(phonetic_list)
+def generate_phonetic():
+    user_word = input("Enter a word:").upper()
+    try:
+        phonetic_list = [nato_dict[word] for word in user_word]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        generate_phonetic()
+    else:
+        print(phonetic_list)
+
+
+generate_phonetic()
